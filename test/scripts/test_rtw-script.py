b51af51a6adec48fd069fe630c15cbedab75440f
Lock9down <patrick.c.loughnane2@gmail.com>
2025-02-09 14:52:21 -0500

    adding testing

---
+++ test/scripts/test_rtw-script.py
@@ -0,0 +1,7 @@
+import pytest
+@pytest.fixture
+#https://www.geeksforgeeks.org/pytest-tutorial-testing-python-application-using-pytest/
+# def my_fixture():
+#     return [1, 2, 3]
+# def test_sum(sample_data):
+#     assert sum(sample_data) == 6
